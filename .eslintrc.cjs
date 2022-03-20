32db8bf80f294afd5ea7380c2756d0db57ecd32f
Ari Perkkiö <ari.perkkio@gmail.com>
2022-03-20 16:43:04 +0000

    feat: add work-around for vitest-dev/vitest#991

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,9 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'prettier',
     ],
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
     overrides: [
         {
             files: require('./tsconfig.json').include,
