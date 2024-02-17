998f5db0415e52b233a4d141327ef3375d1512e5
Ari Perkkiö <ari.perkkio@gmail.com>
2024-02-17 10:48:06 +0200

    feat: support `onWritePath` option (#154)

--- test/index.test.ts
+++ test/index.test.ts
@@ -68,6 +68,65 @@ test('writes a report', async () => {
     `);
 });
 
+test('file path can be rewritten using options.onWritePath ', async () => {
+    await runVitest({
+        reporterOptions: {
+            onWritePath(path: string) {
+                return `custom-prefix/${path}`;
+            },
+        },
+    });
+
+    const contents = readFileSync(outputFile, 'utf-8');
+    const stable = stabilizeReport(contents);
+
+    expect(stable).toMatchInlineSnapshot(`
+      "<testExecutions version="1">
+        <file path="custom-prefix/test/fixtures/animals.test.ts">
+          <testCase name="animals - dogs say woof" duration="123" />
+          <testCase name="animals - figure out what rabbits say" duration="123">
+            <skipped message="figure out what rabbits say" />
+          </testCase>
+          <testCase name="animals - flying ones - cat can fly" duration="123">
+            <failure message="expected false to be true // Object.is equality">
+              <![CDATA[AssertionError: expected false to be true // Object.is equality
+          at <process-cwd>/test/fixtures/animals.test.js
+          <removed-stacktrace>
+            </failure>
+          </testCase>
+          <testCase name="animals - flying ones - bird can fly" duration="123" />
+        </file>
+        <file path="custom-prefix/test/fixtures/math.test.ts">
+          <testCase name="math - sum" duration="123" />
+          <testCase name="math - multiply" duration="123" />
+          <testCase name="math - slow calculation" duration="123" />
+          <testCase name="math - tricky calculation of &quot;16 / 4&quot;" duration="123">
+            <failure message="expected 4 to deeply equal 8">
+              <![CDATA[AssertionError: expected 4 to deeply equal 8
+          at <process-cwd>/test/fixtures/math.test.js
+          <removed-stacktrace>
+            </failure>
+          </testCase>
+          <testCase name="math - complex calculation" duration="123">
+            <error message="16.divideByTwo is not a function">
+              <![CDATA[TypeError: 16.divideByTwo is not a function
+          at <process-cwd>/test/fixtures/math.test.js
+          <removed-stacktrace>
+            </error>
+          </testCase>
+          <testCase name="math - random numbers are unstable" duration="123">
+            <skipped message="random numbers are unstable" />
+          </testCase>
+          <testCase name="math - learn square roots" duration="123">
+            <skipped message="learn square roots" />
+          </testCase>
+          <testCase name="math - divide - basic" duration="123" />
+          <testCase name="math - divide - by zero" duration="123" />
+        </file>
+      </testExecutions>"
+    `);
+});
+
 test('report location is logged', async () => {
     const spy = vi.spyOn(console, 'log');
     await runVitest();
